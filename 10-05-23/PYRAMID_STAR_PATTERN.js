5767b02f2a2d591803322907b643a0ae7bf014bf
Swathi Chatrathi <85331659+swathichatrathi@users.noreply.github.com>
2023-05-14 11:55:05 +0530

    Create PYRAMID_STAR_PATTERN.js

---
+++ 10-05-23/PYRAMID_STAR_PATTERN.js
@@ -0,0 +1,29 @@
+/* Write a js program to print pyramid star pattern as follows 
+    *
+   ***
+  *****
+ *******
+*********
+*/
+
+// code
+
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= 5 - i; j++) {
+    row += " ";
+  }
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+
+// output 
+
+    *
+   ***
+  *****
+ *******
+*********
